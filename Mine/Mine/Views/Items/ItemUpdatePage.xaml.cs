82c94ec5e5a76cfcaaf77fb347e5ae684d2ca692
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-01-24 18:43:01 -0800

    Added Constructor that takes a viewmodel to ItemUpdatePage.xaml.cs

--- Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
+++ Mine/Mine/Views/Items/ItemUpdatePage.xaml.cs
@@ -5,6 +5,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 using Mine.Models;
+using Mine.ViewModels;
 
 namespace Mine.Views
 {
@@ -26,6 +27,18 @@ namespace Mine.Views
             };
 
             BindingContext = this;
+        }
+
+        /// <summary>
+        /// Constructor that takes a ViewModel
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public ItemUpdatePage(ItemReadViewModel viewModel)
+        {
+            InitializeComponent();
+            Item = viewModel.Item;
+
+            BindingContext = this; 
         }
 
         /// <summary>
