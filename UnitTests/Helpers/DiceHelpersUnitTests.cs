394b16074a292413cc820ba23d1f594df1d957f0
Lindsjar37 <55968751+Lindsjar37@users.noreply.github.com>
2021-02-10 11:07:59 -0800

    Added RollDice Invalid Roll 1 dice 0 sdies to DiceHelpersUnitTests.cs

--- UnitTests/Helpers/DiceHelpersUnitTests.cs
+++ UnitTests/Helpers/DiceHelpersUnitTests.cs
@@ -39,6 +39,20 @@ namespace UnitTests.Helpers
             Assert.AreEqual(0, result);
         }
 
+        [Test]
+        public void RollDice_Invalid_Roll_1_Dice_0_Should_Return_Zero()
+        {
+            // Arrange
+
+            // Act
+            var result = DiceHelper.RollDice(1, 0);
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(0, result);
+        }
+
 
         [Test]
         public void RollDice_Valid_Roll_1_Dice_6_Should_Return_Between_1_And_6()
